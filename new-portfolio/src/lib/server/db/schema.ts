cfa2807c6d5a48d38a0bf566e1e351782785ec84
Jemo69 <jemolife69@gmail.com>
2025-10-01 18:08:18 -0400

    added the soem things

--- new-portfolio/src/lib/server/db/schema.ts
+++ new-portfolio/src/lib/server/db/schema.ts
@@ -1,6 +1,16 @@
-import { sqliteTable, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, integer , text    } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
 	id: integer('id').primaryKey(),
-	age: integer('age')
+    email : text("email").notNull(),  
+    password : text("password").notNull()
 });
+export const blog = sqliteTable('blog' , {
+
+    id : integer('id'), 
+    title : text('title'),
+    slug : text('slug').unique().notNull(),
+    cotent : text('content'),
+    
+    }
+)
