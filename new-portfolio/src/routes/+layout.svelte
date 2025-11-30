481e648e3fa93a5c299e76143d02b6df2e39c067
Jemo69 <jemolife69@gmail.com>
2025-11-29 19:52:29 -0500

    new favcon

--- new-portfolio/src/routes/+layout.svelte
+++ new-portfolio/src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+
 	import Navbar from '$lib/components/ui/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/logo.ico" />
 </svelte:head>
 
 <div
