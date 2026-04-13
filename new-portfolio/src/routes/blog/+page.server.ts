bdb58d8542dc6d67f805b49f4f6675750476ed9f
Jemo69 <jemolife69@gmail.com>
2026-04-13 19:32:42 -0400

    chore: sort blog posts from newest to oldest

--- new-portfolio/src/routes/blog/+page.server.ts
+++ new-portfolio/src/routes/blog/+page.server.ts
@@ -3,6 +3,7 @@ import { db } from '$lib/server/db';
 import { blog } from '$lib/server/db/schema';
 import { desc } from 'drizzle-orm';
 
+// Sorting blog posts by creation date (newest first)
 export const load: PageServerLoad = async () => {
 	const posts = await db.select().from(blog).orderBy(desc(blog.createdAt));
 	return { posts };
